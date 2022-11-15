d74948612e8d220729eb12ee7ef715c4f5285815
Atakan Madanoğlu <ati64.98.35@gmail.com>
2022-11-15 10:14:08 +0300

    Implemented NotesModule for di

---
+++ app/src/main/java/com/atakanmadanoglu/notesapplication/di/NotesModule.kt
@@ -0,0 +1,33 @@
+package com.atakanmadanoglu.notesapplication.di
+
+import com.atakanmadanoglu.notesapplication.data.NotesDatabase
+import com.atakanmadanoglu.notesapplication.data.local.NotesDao
+import com.atakanmadanoglu.notesapplication.data.local.NotesLocalDataSource
+import com.atakanmadanoglu.notesapplication.data.repository.NotesRepository
+import com.atakanmadanoglu.notesapplication.data.repository.NotesRepositoryImp
+import dagger.Binds
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class NotesModule {
+    companion object {
+        @Provides
+        fun provideNotesDao(
+            database: NotesDatabase
+        ): NotesDao = database.getNotesDao()
+
+        @Provides
+        fun provideNotesLocalDataSource(
+            dao: NotesDao
+        ) = NotesLocalDataSource(dao)
+    }
+
+    @Binds
+    abstract fun provideNotesRepository(
+        notesRepositoryImp: NotesRepositoryImp
+    ): NotesRepository
+}
