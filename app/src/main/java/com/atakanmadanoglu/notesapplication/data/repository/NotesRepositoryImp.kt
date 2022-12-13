0aeaf741d1fdf04c5821a518189e27aca2fdfa93
Atakan Madanoğlu <ati64.98.35@gmail.com>
2022-12-13 22:57:12 +0300

    Specified dispatcher type of functions on repo

--- app/src/main/java/com/atakanmadanoglu/notesapplication/data/repository/NotesRepositoryImp.kt
+++ app/src/main/java/com/atakanmadanoglu/notesapplication/data/repository/NotesRepositoryImp.kt
@@ -2,14 +2,19 @@ package com.atakanmadanoglu.notesapplication.data.repository
 
 import com.atakanmadanoglu.notesapplication.data.local.NotesDao
 import com.atakanmadanoglu.notesapplication.data.mapper.NoteEntityMapper
+import com.atakanmadanoglu.notesapplication.di.IoDispatcher
 import com.atakanmadanoglu.notesapplication.domain.model.NoteDomain
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class NotesRepositoryImp @Inject constructor(
     private val notesDao: NotesDao,
-    private val noteMapper: NoteEntityMapper
+    private val noteMapper: NoteEntityMapper,
+    @IoDispatcher private val ioDispatcher: CoroutineDispatcher
 ) : NotesRepository {
     override fun getNotesByCreatedAt(): Flow<List<NoteDomain>> =
         notesDao.getNotesByCreatedAt().map { notesList ->
@@ -21,14 +26,14 @@ class NotesRepositoryImp @Inject constructor(
     override fun getNoteById(id: Int): Flow<NoteDomain> =
         notesDao.getNoteById(id).map {
             noteMapper.mapToNoteDomain(it)
-        }
+        }.flowOn(ioDispatcher)
 
-    override suspend fun addNote(noteDomain: NoteDomain) {
+    override suspend fun addNote(noteDomain: NoteDomain) = withContext(ioDispatcher) {
         val noteEntity = noteMapper.mapToNoteEntity(noteDomain)
         notesDao.addNote(noteEntity)
     }
 
-    override suspend fun editNote(noteDomain: NoteDomain) {
+    override suspend fun editNote(noteDomain: NoteDomain) = withContext(ioDispatcher) {
         val noteEntity = noteMapper.mapToNoteEntity(noteDomain)
         notesDao.editNote(noteEntity)
     }
