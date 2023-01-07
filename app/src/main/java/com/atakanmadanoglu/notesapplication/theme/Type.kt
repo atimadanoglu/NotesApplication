fd27f2d075c7347c2e4becaa60b0a66b0c6553a0
Atakan Madanoğlu <ati64.98.35@gmail.com>
2023-01-07 14:24:35 +0300

    Added new font families

--- app/src/main/java/com/atakanmadanoglu/notesapplication/theme/Type.kt
+++ app/src/main/java/com/atakanmadanoglu/notesapplication/theme/Type.kt
@@ -2,9 +2,42 @@ package com.atakanmadanoglu.notesapplication.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.atakanmadanoglu.notesapplication.R
+
+val openSansSemiBoldFontFamily = FontFamily(
+    Font(resId = R.font.open_sans_semi_bold)
+)
+
+val openSansLightFontFamily = FontFamily(
+    Font(resId = R.font.open_sans_light)
+)
+
+val openSansRegularFontFamily = FontFamily(
+    Font(resId = R.font.open_sans_regular)
+)
+
+val openSansLightItalicFontFamily = FontFamily(
+    Font(resId = R.font.open_sans_light_italic)
+)
+
+val Typography.openSansSemiBold: TextStyle get() = TextStyle(
+    fontFamily = openSansSemiBoldFontFamily
+)
+val Typography.openSansLight: TextStyle get() = TextStyle(
+    fontFamily = openSansLightFontFamily
+)
+
+val Typography.openSansRegular: TextStyle get() = TextStyle(
+    fontFamily = openSansRegularFontFamily
+)
+
+val Typography.openSansLightItalic: TextStyle get() = TextStyle(
+    fontFamily = openSansLightItalicFontFamily
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -15,20 +48,4 @@ val Typography = Typography(
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
     )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
 )
