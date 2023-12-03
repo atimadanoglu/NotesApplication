8813e8562e17e0a8abcb94fa1d17501924e80629
Atakan Madanoğlu <ati64.98.35@gmail.com>
2023-12-03 12:42:14 +0300

    Remove an unused navigation method

--- app/src/main/java/com/atakanmadanoglu/notesapplication/presentation/notes_list/navigation/NotesListNavigation.kt
+++ app/src/main/java/com/atakanmadanoglu/notesapplication/presentation/notes_list/navigation/NotesListNavigation.kt
@@ -1,16 +1,11 @@
 package com.atakanmadanoglu.notesapplication.presentation.notes_list.navigation
 
-import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import com.atakanmadanoglu.notesapplication.presentation.navigation.Screen
 import com.atakanmadanoglu.notesapplication.presentation.notes_list.NoteListRoute
 
-fun NavController.navigateToNotesListScreen() {
-    this.navigate(Screen.NOTE_LIST.route)
-}
-
-fun NavGraphBuilder.noteList(
+fun NavGraphBuilder.noteListScreen(
     navigateToAddNoteScreen: () -> Unit,
     navigateToEditNoteScreen: (Int) -> Unit
 ) {
